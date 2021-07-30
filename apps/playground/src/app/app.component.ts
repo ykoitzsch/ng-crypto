1cba0f03f05abd7670185f0db2d486c3d8788f44
ykoitzsch <y.koitzsch@gmx.at>
2021-07-30 20:55:34 +0200

    initial commit

---
+++ apps/playground/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'isleepcode-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'playground';
+}
